c0b5045a1ac2a2a0ca89a315268644c910a3dfa0
Rajat Gupta <rajat295gupta@gmail.com>
2019-12-31 10:01:49 +0530

    email avail bugFix

--- public/js/addUser.js
+++ public/js/addUser.js
@@ -7,6 +7,7 @@ var user_role = document.getElementById('user_role');
 var user_gender = document.getElementById('user_gender')
 var adding = document.getElementById('submit-btn');
 var cancel_addUser = document.getElementById('cancel-btn');
+var flag = 0;
 
 adding.addEventListener("click", function() {  
 
@@ -75,25 +76,43 @@ adding.addEventListener("click", function() {
 	obj.photoname = '/default.png'
 	obj.flag = 1;
 
-   	var request = new XMLHttpRequest();
-    request.open('POST',"/admin/addnewuser");
-    request.setRequestHeader("Content-Type","application/json");
-    request.send(JSON.stringify(obj))
-    request.addEventListener("load",function() {
-         $.confirm({
-	    	title: 'New User ?',
-	    	content: "New User is Registered ",
-	    	draggable: true,
-	   		buttons: {
-	        OK: {
-	            btnClass: 'btn-danger any-other-class',
-	             action: function () {  
-	             location.reload();    
-	        	}
-	   		},
-	    	}
-		});
-    });  
+	if(flag == 1)
+	{
+		$.confirm({
+		      title: 'User ?',
+		      content: "User Is Already Registred !! ",
+		      draggable: true,
+		      buttons: {
+		        OK: {
+		            btnClass: 'btn-danger any-other-class',
+		             action: function () { 
+		          }
+		          },
+		          }
+		    });
+	}
+	else
+	{
+		var request = new XMLHttpRequest();
+	    request.open('POST',"/admin/addnewuser");
+	    request.setRequestHeader("Content-Type","application/json");
+	    request.send(JSON.stringify(obj))
+	    request.addEventListener("load",function() {
+	         $.confirm({
+		    	title: 'New User ?',
+		    	content: "New User is Registered ",
+		    	draggable: true,
+		   		buttons: {
+		        OK: {
+		            btnClass: 'btn-danger any-other-class',
+		             action: function () {  
+		             location.reload();    
+		        	}
+		   		},
+		    	}
+			});
+	    });
+	}
 })
 
 function ValidateEmail(mail) 
@@ -108,3 +127,28 @@ function ValidateEmail(mail)
 cancel_addUser.addEventListener("click", function(){
 	location.reload();
 })
+
+
+function email_avail()
+{
+	document.getElementById("email_info").style.display = 'visible';
+	document.getElementById("email_info").style.display = 'block';
+	document.getElementById("email_info").style.marginTop = '10px';
+	document.getElementById("email_info").style.marginBottom = '10px';
+	
+	var request = new XMLHttpRequest();
+    request.open('POST',"/admin/checkemail");
+    request.setRequestHeader("Content-Type","application/json");
+    request.send(JSON.stringify({email: user_email.value}));
+    request.addEventListener("load",function() {
+    	var data = request.responseText;
+    	if(data === 'true') {
+    		display_email.innerHTML= "User " + user_email.value + " is already exist";
+    		flag = 1;
+    	}
+    	else {
+            display_email.innerHTML= user_email.value + " is available";
+            flag = 0;
+    	}
+    });  
+}
