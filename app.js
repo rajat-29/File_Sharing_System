5af30efd2d787ee979902d51825c71c74e18fa16
Rajat Gupta <rajat295gupta@gmail.com>
2020-05-11 17:40:45 +0530

    include

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express = require('express')
 var path = require('path')
 var app = express()
 var session = require('express-session');
+var engine = require("ejs-mate");
 var port=3000;
 
-app.set('views', path.join(__dirname, 'Views')); 
+app.engine("ejs", engine);
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'Views'));
+
 app.use(express.static(path.join(__dirname,'/public'))) 
 app.use(express.static(path.join(__dirname,'/public/uploads')))
 
